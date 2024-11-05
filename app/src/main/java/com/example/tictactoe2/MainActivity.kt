bafbcc6d18f605fdcd6364e15b3a67e725cd7bd6
Sampath Guruprasad <sampathguruprasad@gmail.com>
2024-11-06 00:45:42 +0530

    Added next page. After clicking on Start button. Flag this for further review and modification

--- app/src/main/java/com/example/tictactoe2/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe2/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -26,9 +28,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tictactoe2.ui.theme.TicTacToe2Theme
 
 class MainActivity : ComponentActivity() {
@@ -52,7 +59,10 @@ fun MyApp(modifier:Modifier=Modifier){
             HomePage(
                 onStartClicked = {shouldShowStartPage=false})
         }
-        else if(shouldShowSettingPage){
+        else{
+            GameOptionsPage(modifier)
+        }
+        if(shouldShowSettingPage){
 
         }
     }
@@ -63,6 +73,57 @@ fun MyApp(modifier:Modifier=Modifier){
 fun MyAppPreview(modifier:Modifier=Modifier){
     MyApp()
 }
+
+
+
+@Composable
+fun GameOptionsPage(modifier:Modifier=Modifier){
+    var shouldShowThreeGrid by rememberSaveable { mutableStateOf(true) }
+    var shouldShowEightGrid by rememberSaveable { mutableStateOf(true) }
+    Surface (modifier,color = Color.DarkGray) {
+        if (shouldShowThreeGrid) {
+            GameOptions(
+                onStartClicked = { shouldShowThreeGrid = false }
+            )
+        }
+    }
+}
+
+@Composable
+fun GameOptions(onStartClicked: () -> Unit,modifier:Modifier=Modifier){
+    val buttonSize = Modifier.size(200.dp,60.dp)
+
+    Column(modifier=modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally){
+        Text(
+            text = "Please Select the Grid",
+            fontSize = 33.sp,
+            modifier = Modifier.padding(bottom = 90.dp),
+            style = MaterialTheme.typography.headlineMedium,
+            color = Color.White,
+            fontWeight = FontWeight.ExtraBold,
+            fontStyle = FontStyle.Normal
+        )
+        ElevatedButton(modifier = buttonSize,
+            onClick = onStartClicked) {
+            Text(
+                text = "3 x 3",
+                fontWeight = FontWeight.ExtraBold,
+                style = MaterialTheme.typography.headlineMedium
+            )
+        }
+        ElevatedButton(modifier = buttonSize.padding(top = 6.dp),
+            onClick = onStartClicked){
+            Text(
+                text = "8 x 8",
+                fontWeight = FontWeight.ExtraBold,
+                style = MaterialTheme.typography.headlineMedium
+            )
+        }
+    }
+}
+
 
 
 
@@ -117,4 +178,9 @@ fun GreetingPreview() {
     TicTacToe2Theme {
         Greeting("Android")
     }
+}
+@Preview(showBackground = true)
+@Composable
+fun GameOptionsPreview(){
+    GameOptionsPage()
 }
